33485c3729ceb7cbbe31c6dba9eb2abe92a05131
Zipho Matiso <ziphomts@gmail.com>
2019-03-19 12:03:20 -0700

    First change

--- zipho/sorting.py
+++ zipho/sorting.py
@@ -9,6 +9,22 @@ def bubble_sort(items):
     return out
 
 
+def merge(A, B):
+   new_list = []
+   while len(A) > 0 and len(B) > 0:
+       if A[0] < B[0]:
+           new_list.append(A[0])
+           A.pop(0)
+       else:
+           new_list.append(B[0])
+           B.pop(0)
+   if len(A) == 0:
+       new_list = new_list + B
+   if len(B) == 0:
+       new_list = new_list + A
+   return new_list
+
+
 def merge_sort(items):
 
     len_i = len(items)
